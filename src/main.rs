bba88195514d2b84e621516cc0bab58c2f3dd7d4
Nicolas Barrios <nbarrios1337@gmail.com>
2023-05-26 02:52:11 -0400

    Implement struct packing manually

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,20 @@ struct DNS_Header {
     num_additionals: u16,
 }
 
+impl DNS_Header {
+    fn to_bytes(self) -> Vec<u8> {
+        // 6 fields, 2 bytes each
+        let mut buf: Vec<u8> = Vec::with_capacity(6 * 2);
+        buf.extend_from_slice(&self.id.to_be_bytes());
+        buf.extend_from_slice(&self.flags.to_be_bytes());
+        buf.extend_from_slice(&self.num_questions.to_be_bytes());
+        buf.extend_from_slice(&self.num_answers.to_be_bytes());
+        buf.extend_from_slice(&self.num_authorities.to_be_bytes());
+        buf.extend_from_slice(&self.num_additionals.to_be_bytes());
+        buf
+    }
+}
+
 #[derive(Debug, Clone)]
 struct DomainName(String);
 
@@ -20,7 +34,7 @@ impl From<String> for DomainName {
 }
 
 impl DomainName {
-    fn encode_dns_name(self) -> Vec<u8> {
+    fn encode_dns_name(&self) -> Vec<u8> {
         let mut encoded: Vec<u8> = self
             .0
             .split('.')
@@ -45,9 +59,18 @@ struct DNS_Question {
     r#type: u16, // TODO definitely a future enum
 }
 
+impl DNS_Question {
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut buf = self.name.encode_dns_name();
+        buf.extend_from_slice(&self.r#type.to_be_bytes());
+        buf.extend_from_slice(&self.class.to_be_bytes());
+        buf
+    }
+}
+
 const CLASS_IN: u16 = 1;
 const TYPE_A: u16 = 1;
-pub fn build_query(domain_name: &str, record_type: u16) {
+pub fn build_query(domain_name: &str, record_type: u16) -> Vec<u8> {
     let id: u16 = rand::thread_rng().gen();
     // endianness clarification: 7th MSB of the 3rd octet is 9 bits away from bit 15.
     const RECURSION_DESIRED: u16 = 1 << 8;
@@ -66,6 +89,13 @@ pub fn build_query(domain_name: &str, record_type: u16) {
         class: CLASS_IN,
         r#type: record_type,
     };
+
+    let mut header_bytes = header.to_bytes();
+    let mut question_bytes = question.to_bytes();
+    let mut buf = Vec::with_capacity(header_bytes.len() + question_bytes.len());
+    buf.append(&mut header_bytes);
+    buf.append(&mut question_bytes);
+    buf
 }
 
 fn main() {
@@ -85,5 +115,29 @@ mod tests {
         let result_bytes = google_domain.encode_dns_name();
 
         assert_eq!(result_bytes, correct_bytes);
+    }
+
+    #[test]
+    fn test_build_query() -> std::fmt::Result {
+        let correct_bytes_str =
+            "82980100000100000000000003777777076578616d706c6503636f6d0000010001";
+        let query_bytes = build_query("www.example.com", TYPE_A);
+
+        let mut query_bytes_str = String::with_capacity(correct_bytes_str.len());
+
+        use std::fmt::Write;
+        for byte in query_bytes {
+            write!(&mut query_bytes_str, "{byte:02x}")?;
+        }
+
+        // Skip first 2 bytes (random id)
+        // 2 chars per byte as formatted above --> 4 chars to skip
+        assert_eq!(
+            query_bytes_str[4..],
+            correct_bytes_str[4..],
+            "Byte value mismatch"
+        );
+
+        Ok(())
     }
 }
