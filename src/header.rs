34a272ccf4a0c2b8955950530151d1c35c3aaa64
Nicolas Barrios <nbarrios1337@gmail.com>
2023-06-01 18:33:13 -0400

    Add decoding fn for header

--- src/header.rs
+++ src/header.rs
@@ -20,4 +20,27 @@ impl DnsHeader {
         buf.extend_from_slice(&self.num_additionals.to_be_bytes());
         buf
     }
+
+    pub fn from_bytes(bytes: &[u8]) -> Self {
+        // header is only twelve bytes
+        let buf: [u8; 12] = bytes[..12]
+            .try_into()
+            .expect("Not enough bytes of header information");
+
+        let id = u16::from_be_bytes(buf[0..2].try_into().unwrap());
+        let flags = u16::from_be_bytes(buf[2..4].try_into().unwrap());
+        let num_questions = u16::from_be_bytes(buf[4..6].try_into().unwrap());
+        let num_answers = u16::from_be_bytes(buf[6..8].try_into().unwrap());
+        let num_authorities = u16::from_be_bytes(buf[8..10].try_into().unwrap());
+        let num_additionals = u16::from_be_bytes(buf[10..12].try_into().unwrap());
+
+        Self {
+            id,
+            flags,
+            num_questions,
+            num_answers,
+            num_authorities,
+            num_additionals,
+        }
+    }
 }
