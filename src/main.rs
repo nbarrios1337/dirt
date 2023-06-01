235926232008c1debc1efab8b8aeede322a7c243
Nicolas Barrios <nbarrios1337@gmail.com>
2023-06-01 03:04:25 +0000

    Refactor struct defs into separate file

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,8 @@
+mod types;
+
 use rand::Rng;
 
-#[derive(Debug, Clone, Copy)] // TODO what other derives needed?
-struct DnsHeader {
-    id: u16,
-    flags: u16, // TODO bitflags?
-    num_questions: u16,
-    num_answers: u16,
-    num_authorities: u16,
-    num_additionals: u16,
-}
-
-impl DnsHeader {
-    fn to_bytes(self) -> Vec<u8> {
-        // 6 fields, 2 bytes each
-        let mut buf: Vec<u8> = Vec::with_capacity(6 * 2);
-        buf.extend_from_slice(&self.id.to_be_bytes());
-        buf.extend_from_slice(&self.flags.to_be_bytes());
-        buf.extend_from_slice(&self.num_questions.to_be_bytes());
-        buf.extend_from_slice(&self.num_answers.to_be_bytes());
-        buf.extend_from_slice(&self.num_authorities.to_be_bytes());
-        buf.extend_from_slice(&self.num_additionals.to_be_bytes());
-        buf
-    }
-}
-
-#[derive(Debug, Clone)]
-struct DomainName(String);
-
-impl From<String> for DomainName {
-    fn from(value: String) -> Self {
-        DomainName(value)
-    }
-}
-
-impl DomainName {
-    fn encode_dns_name(&self) -> Vec<u8> {
-        let mut encoded: Vec<u8> = self
-            .0
-            .split('.')
-            .map(|substr| (substr.len(), substr.to_string()))
-            .flat_map(|(len, mut substr)| {
-                substr.insert(0, len as u8 as char);
-                substr.into_bytes()
-            })
-            .collect();
-        encoded.push(0);
-        encoded
-    }
-
-    pub fn new(domain_name: &str) -> Self {
-        DomainName(domain_name.to_string())
-    }
-}
-#[derive(Debug, Clone)]
-struct DnsQuestion {
-    name: DomainName,
-    class: u16,
-    r#type: u16, // TODO definitely a future enum
-}
-
-impl DnsQuestion {
-    fn to_bytes(&self) -> Vec<u8> {
-        let mut buf = self.name.encode_dns_name();
-        buf.extend_from_slice(&self.r#type.to_be_bytes());
-        buf.extend_from_slice(&self.class.to_be_bytes());
-        buf
-    }
-}
+use types::*;
 
 const CLASS_IN: u16 = 1;
 const TYPE_A: u16 = 1;
---
+++ src/types.rs
@@ -0,0 +1,67 @@
+#[derive(Debug, Clone, Copy)] // TODO what other derives needed?
+pub struct DnsHeader {
+    pub id: u16,
+    pub flags: u16, // TODO bitflags?
+    pub num_questions: u16,
+    pub num_answers: u16,
+    pub num_authorities: u16,
+    pub num_additionals: u16,
+}
+
+impl DnsHeader {
+    pub fn to_bytes(self) -> Vec<u8> {
+        // 6 fields, 2 bytes each
+        let mut buf: Vec<u8> = Vec::with_capacity(6 * 2);
+        buf.extend_from_slice(&self.id.to_be_bytes());
+        buf.extend_from_slice(&self.flags.to_be_bytes());
+        buf.extend_from_slice(&self.num_questions.to_be_bytes());
+        buf.extend_from_slice(&self.num_answers.to_be_bytes());
+        buf.extend_from_slice(&self.num_authorities.to_be_bytes());
+        buf.extend_from_slice(&self.num_additionals.to_be_bytes());
+        buf
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct DomainName(String);
+
+impl From<String> for DomainName {
+    fn from(value: String) -> Self {
+        DomainName(value)
+    }
+}
+
+impl DomainName {
+    pub fn encode_dns_name(&self) -> Vec<u8> {
+        let mut encoded: Vec<u8> = self
+            .0
+            .split('.')
+            .map(|substr| (substr.len(), substr.to_string()))
+            .flat_map(|(len, mut substr)| {
+                substr.insert(0, len as u8 as char);
+                substr.into_bytes()
+            })
+            .collect();
+        encoded.push(0);
+        encoded
+    }
+
+    pub fn new(domain_name: &str) -> Self {
+        DomainName(domain_name.to_string())
+    }
+}
+#[derive(Debug, Clone)]
+pub struct DnsQuestion {
+    pub name: DomainName,
+    pub class: u16,
+    pub r#type: u16, // TODO definitely a future enum
+}
+
+impl DnsQuestion {
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut buf = self.name.encode_dns_name();
+        buf.extend_from_slice(&self.r#type.to_be_bytes());
+        buf.extend_from_slice(&self.class.to_be_bytes());
+        buf
+    }
+}
