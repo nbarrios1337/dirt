b06fa1a76062932ecfd340dbba55e325c3e687b2
Nicolas Barrios <nbarrios1337@gmail.com>
2023-06-02 13:07:25 -0400

    [WIP] first pass at name decoding

--- src/dname.rs
+++ src/dname.rs
@@ -1,6 +1,12 @@
 #[derive(Debug, Clone)]
 pub struct DomainName(String);
 
+impl DomainName {
+    const MAX_LABEL_SIZE: usize = 63;
+    const MAX_NAME_SIZE: usize = 255;
+    const MAX_UDP_MSG_SIZE: usize = 512;
+}
+
 impl From<String> for DomainName {
     fn from(value: String) -> Self {
         DomainName(value)
@@ -22,7 +28,50 @@ impl DomainName {
         encoded
     }
 
+    pub fn decode_dns_name(mut bytes: &[u8]) -> Result<Self> {
+        use std::io::prelude::*;
+        let mut labels = Vec::new();
+
+        let mut label_bytes_buffer: Vec<u8> = Vec::with_capacity(Self::MAX_LABEL_SIZE);
+
+        // while the length octet is not zero
+        while bytes[0] != 0 {
+            let cur_length: usize = bytes[0] as usize;
+            // get exactly the amt of bytes for the current label
+            bytes
+                .read_exact(&mut label_bytes_buffer[..cur_length])
+                .map_err(DomainNameError::Io)?;
+
+            let bytes_to_convert = &label_bytes_buffer[..cur_length];
+
+            let cur_label =
+                std::str::from_utf8(bytes_to_convert).map_err(DomainNameError::Parse)?;
+            labels.push(cur_label);
+        }
+
+        Ok(Self(labels.join(".")))
+    }
+
     pub fn new(domain_name: &str) -> Self {
         DomainName(domain_name.to_string())
     }
 }
+
+type Result<T> = std::result::Result<T, DomainNameError>;
+
+#[derive(Debug)]
+enum DomainNameError {
+    Io(std::io::Error),
+    Parse(std::str::Utf8Error),
+}
+
+impl std::fmt::Display for DomainNameError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DomainNameError::Io(io) => write!(f, "IO error: {io}"),
+            DomainNameError::Parse(parse) => write!(f, "String parsing error: {parse}"),
+        }
+    }
+}
+
+impl std::error::Error for DomainNameError {}
