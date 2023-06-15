a3c935d62d826d76e3c6456573bc54d1bbeddcee
Nicolas Barrios <nbarrios1337@gmail.com>
2023-06-15 14:37:47 -0400

    Extract query functionality to function

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod qtype;
 mod question;
 mod record;
 
-use std::io::Cursor;
+use std::{io::Cursor, net::UdpSocket};
 
 use rand::Rng;
 
@@ -15,9 +15,8 @@ use header::Header;
 use packet::Packet;
 use qtype::QType;
 use question::Question;
-use record::Record;
 
-pub fn build_query(domain_name: &str, record_type: u16) -> Vec<u8> {
+pub fn build_query(domain_name: &str, record_type: QType) -> Vec<u8> {
     let id: u16 = rand::thread_rng().gen();
     // endianness clarification: 7th MSB of the 3rd octet is 9 bits away from bit 15.
     const RECURSION_DESIRED: u16 = 1 << 8;
@@ -34,7 +33,7 @@ pub fn build_query(domain_name: &str, record_type: u16) -> Vec<u8> {
     let question = Question {
         qname: name,
         qclass: qclass::QClass::IN,
-        qtype: QType::try_from(record_type).unwrap(),
+        qtype: record_type,
     };
 
     let mut header_bytes = header.to_bytes();
@@ -45,44 +44,40 @@ pub fn build_query(domain_name: &str, record_type: u16) -> Vec<u8> {
     buf
 }
 
-fn main() -> std::io::Result<()> {
-    use std::net::UdpSocket;
-
-    let query_bytes = build_query("www.example.com", qtype::QType::A.into());
-    let dns_server_addr = "8.8.8.8:53";
+fn lookup_domain(domain_name: &str) -> Result<std::net::Ipv4Addr, packet::PacketError> {
+    let query = build_query(domain_name, qtype::QType::A);
 
     // connection setup
     let udp_sock = UdpSocket::bind((std::net::Ipv4Addr::UNSPECIFIED, 0))
         .unwrap_or_else(|e| panic!("Couldn't bind to local address -- {e}"));
-
+    let dns_server_addr = "8.8.8.8:53";
     udp_sock
         .connect(dns_server_addr)
         .unwrap_or_else(|e| panic!("Couldn't connect to DNS Server @ {dns_server_addr} -- {e}"));
 
     // query request
-    udp_sock.send(&query_bytes).expect("Couldn't send query");
+    udp_sock.send(&query).expect("Couldn't send query");
 
-    // query response
+    // get response
     let mut recv_buf = [0u8; 1024];
-    match udp_sock.recv(&mut recv_buf) {
-        Ok(bytes_sent) => {
-            let recv_bytes = &recv_buf[..bytes_sent];
-            print_bytes_as_hex(recv_bytes);
-        }
-        Err(e) => {
-            let mut query_id_bytes = [0u8; 2];
-            query_id_bytes.clone_from_slice(&query_bytes[0..2]);
-            let query_id = u16::from_be_bytes(query_id_bytes);
-            eprintln!("No response returned for query {query_id} -- {e}");
-        }
+    let bytes_recv = udp_sock
+        .recv(&mut recv_buf)
+        .expect("Reponse receipt failed");
+
+    // parse response to packet
+    let mut pkt_bytes_reader = Cursor::new(&recv_buf[..bytes_recv]);
+
+    // get id addr
+    Packet::from_bytes(&mut pkt_bytes_reader).map(|pkt| {
+        std::net::Ipv4Addr::from(<[u8; 4]>::try_from(&pkt.answers[0].rdata[..4]).unwrap())
+    })
+}
+
+fn main() {
+    match lookup_domain("www.example.com") {
+        Ok(ip) => println!("{ip}"),
+        Err(e) => eprintln!("{e}"),
     }
-
-    let mut recv_reader = Cursor::new(&recv_buf[..]);
-
-    let pkt = Packet::from_bytes(&mut recv_reader);
-    dbg!(pkt);
-
-    Ok(())
 }
 
 fn print_bytes_as_hex(bytes: &[u8]) {
@@ -101,7 +96,7 @@ mod tests {
     fn test_build_query() -> std::fmt::Result {
         let correct_bytes_str =
             "82980100000100000000000003777777076578616d706c6503636f6d0000010001";
-        let query_bytes = build_query("www.example.com", qtype::QType::A.into());
+        let query_bytes = build_query("www.example.com", qtype::QType::A);
 
         let mut query_bytes_str = String::with_capacity(correct_bytes_str.len());
 
@@ -139,7 +134,7 @@ mod tests {
 
     #[test]
     fn test_send_query() -> std::io::Result<()> {
-        let query_bytes = build_query("www.example.com", qtype::QType::A.into());
+        let query_bytes = build_query("www.example.com", qtype::QType::A);
 
         // connection setup
         let udp_sock = socket_setup();
