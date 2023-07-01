6adc901472d24275d73af6c128b22566658a76f9
Nicolas Barrios <nbarrios1337@gmail.com>
2023-06-30 22:04:25 -0400

    Documentation pass for Message

--- src/message.rs
+++ src/message.rs
@@ -8,16 +8,40 @@ use crate::{
     record::{Record, RecordError},
 };
 
+/// All communications inside of the domain protocol are carried in a single format called a message.
+///
+/// The top level format of message is divided
+///  into 5 sections (some of which are empty in certain cases) shown below:
+/// ```text
+/// +---------------------+
+/// |        Header       |
+/// +---------------------+
+/// |       Question      | the question for the name server
+/// +---------------------+
+/// |        Answer       | RRs answering the question
+/// +---------------------+
+/// |      Authority      | RRs pointing toward an authority
+/// +---------------------+
+/// |      Additional     | RRs holding additional information
+/// +---------------------+
+/// ```
 #[derive(Debug)]
 pub struct Message {
     pub header: Header,
+    /// The query name(s) and other query parameters.
     pub questions: Vec<Question>,
+    /// RRs which directly answer the query.
     pub answers: Vec<Record>,
+    /// RRs which describe other authoritative servers.
+    ///
+    /// May optionally carry the SOA RR for the authoritative data in the answer section
     pub authorities: Vec<Record>,
+    /// RRs which may be helpful in using the RRs in the other sections.
     pub additionals: Vec<Record>,
 }
 
 impl Message {
+    /// Reads a [Message] a sequence of bytes
     pub fn from_bytes(bytes: &mut Cursor<&[u8]>) -> MessageResult<Self> {
         let header = Header::from_bytes(bytes)?;
 
